01b7476ae045f8d24f09ca9221dd0b2ed374b036
PaulBcl <paul.bacle@gmail.com>
2025-03-06 11:13:53 +0100

    Update SessionState.py

--- SessionState.py
+++ SessionState.py
@@ -16,34 +16,6 @@ result:
 'Mary'
 """
 
-
-
-#OLD report_thread - ne fonctionne plus avec l'upgrade streamlit >=1.4. https://discuss.streamlit.io/t/modulenotfounderror-no-module-named-streamlit-report-thread/20983/14
-#remplacé par ci-après get_script_run_ctx
-
-try:
-    import streamlit.ReportThread as ReportThread
-    from streamlit.server.Server import Server
-except Exception:
-    # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportThread
-    from streamlit.server.server import Server
-
-"""
-#a essayer plus tard quand upgrade de streamlit mais génère encore des erreurs " 'AppSession' object has no attribute 'enqueue'"
-try:
-    from streamlit.scriptrunner import get_script_run_ctx
-except ModuleNotFoundError:
-    # streamlit < 1.8
-    try:
-        from streamlit.script_run_context import get_script_run_ctx  # type: ignore
-    except ModuleNotFoundError:
-        # streamlit < 1.4
-        from streamlit.report_thread import (  # type: ignore
-            get_report_ctx as get_script_run_ctx,
-        )
-from streamlit.server.server import Server"""
-
 import streamlit as st
 
 class SessionState:
